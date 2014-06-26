49c9183908986547871b334f75901a6e55a2fae7
Aaron Socas Gaspar <alu0100207385@ull.edu.es>
2014-06-26 17:46:42 +0100

    ver0.13.0 (slider testimonials mejorado)

--- js/slide1.js
+++ js/slide1.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
-    $('#testimonial_slider').bxSlider({
-      mode: 'fade',
-      auto: true,
-      autoControls: true,
-      pause: 4000
-    });
+  var slider = $('#testimonial_slider').bxSlider({
+    mode: 'horizontal',
+    speed: 800
+  });
 });
+
+
